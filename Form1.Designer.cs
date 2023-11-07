330c0ad76a52d915b188350f652371c336eb8d7c
DaniBD97 <danibld97@gmail.com>
2023-11-07 01:32:41 -0300

    Version 1

---
+++ Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Promedio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPromedio = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnPromediar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxNota = new System.Windows.Forms.TextBox();
+            this.textBoxPonderacion = new System.Windows.Forms.TextBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtPromedio
+            // 
+            this.txtPromedio.Location = new System.Drawing.Point(421, 482);
+            this.txtPromedio.Name = "txtPromedio";
+            this.txtPromedio.Size = new System.Drawing.Size(187, 20);
+            this.txtPromedio.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(337, 489);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Promedio";
+            // 
+            // btnPromediar
+            // 
+            this.btnPromediar.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPromediar.Location = new System.Drawing.Point(323, 525);
+            this.btnPromediar.Name = "btnPromediar";
+            this.btnPromediar.Size = new System.Drawing.Size(172, 41);
+            this.btnPromediar.TabIndex = 10;
+            this.btnPromediar.Text = "Promediar";
+            this.btnPromediar.UseVisualStyleBackColor = true;
+            this.btnPromediar.Click += new System.EventHandler(this.btnPromediar_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(27, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 41);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Agregar Mas Notas";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(348, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nota 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(535, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ponderacion ";
+            // 
+            // textBoxNota
+            // 
+            this.textBoxNota.Location = new System.Drawing.Point(404, 65);
+            this.textBoxNota.Name = "textBoxNota";
+            this.textBoxNota.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNota.TabIndex = 4;
+            // 
+            // textBoxPonderacion
+            // 
+            this.textBoxPonderacion.Location = new System.Drawing.Point(633, 65);
+            this.textBoxPonderacion.Name = "textBoxPonderacion";
+            this.textBoxPonderacion.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPonderacion.TabIndex = 6;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(546, 519);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnReset.Size = new System.Drawing.Size(168, 47);
+            this.btnReset.TabIndex = 12;
+            this.btnReset.Text = "Limpiar";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 53);
+            this.label3.MaximumSize = new System.Drawing.Size(5000, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(317, 31);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Calculador de Promedios";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(66, 287);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(777, 584);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnPromediar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtPromedio);
+            this.Controls.Add(this.textBoxPonderacion);
+            this.Controls.Add(this.textBoxNota);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtPromedio;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnPromediar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxNota;
+        private System.Windows.Forms.TextBox textBoxPonderacion;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Promedio
+{
+    public partial class Form1 : Form
+    {
+        private List<TextBox> textBoxNotas = new List<TextBox>();
+        private List<TextBox> textBoxPonderaciones = new List<TextBox>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBoxNotas.Add(textBoxNota);
+            textBoxPonderaciones.Add(textBoxPonderacion);
+        }
+
+        private void btnPromediar_Click(object sender, EventArgs e)
+        {
+            int sumaPonderada = 0;
+            int sumaPonderaciones = 0;
+
+            // Incluir el TextBox original si existe
+            if (textBoxNotas.Count > 0 && textBoxPonderaciones.Count > 0)
+            {
+                if (int.TryParse(textBoxNotas[0].Text, out int nota) && int.TryParse(textBoxPonderaciones[0].Text, out int ponderacion))
+                {
+                    sumaPonderada += nota * ponderacion;
+                    sumaPonderaciones += ponderacion;
+                }
+            }
+
+            for (int i = 1; i < textBoxNotas.Count; i++)
+            {
+                if (int.TryParse(textBoxNotas[i].Text, out int nota) && int.TryParse(textBoxPonderaciones[i].Text, out int ponderacion))
+                {
+                    sumaPonderada += nota * ponderacion;
+                    sumaPonderaciones += ponderacion;
+                }
+            }
+
+            if (sumaPonderaciones > 0)
+            {
+                int promedio = sumaPonderada / sumaPonderaciones;
+                txtPromedio.Text = promedio.ToString();
+
+                if (promedio < 30)
+                {
+                    MessageBox.Show("Reprobado");
+                }
+                else
+                {
+                    MessageBox.Show("Aprobado");
+                }
+            }
+            else
+            {
+                MessageBox.Show("No se ingresaron notas o ponderaciones válidas.");
+            }
+        }
+        int indiceLabel = 2;
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Label label = new Label();
+
+            label.Text = "Nota " + indiceLabel;
+            label.Location = new System.Drawing.Point(359, 15 + indiceLabel * 40);
+            label.Width = 40;
+
+            TextBox notaBox = new TextBox();
+            notaBox.Name = "textBoxNota" + textBoxNotas.Count; // Asigna nombres únicos
+            notaBox.Location = new System.Drawing.Point(404, 55 + textBoxNotas.Count * 40);
+
+
+
+            Label label2 = new Label();
+            label2.Text = "Ponderacion  " + indiceLabel;
+            label2.Location = new System.Drawing.Point(557, 15 + indiceLabel * 40);
+            label2.Width = 76;
+            TextBox ponderacionBox = new TextBox();
+            ponderacionBox.Name = "textBoxPonderacion" + textBoxPonderaciones.Count;        
+            ponderacionBox.Location = new System.Drawing.Point(633, 55 + textBoxPonderaciones.Count * 40);
+
+            this.Controls.Add(label);
+            this.Controls.Add(label2);
+            this.Controls.Add(notaBox);
+            this.Controls.Add(ponderacionBox);
+            indiceLabel++;
+            textBoxNotas.Add(notaBox);
+            textBoxPonderaciones.Add(ponderacionBox);
+
+
+            if (textBoxNotas.Count >= 10)
+            {
+                MessageBox.Show("Se ha alcanzado el límite de 10 notas.");
+                button1.Enabled = false; // Desactiva el botón
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            // Restablecer los TextBox de notas y ponderaciones
+            foreach (TextBox notaBox in textBoxNotas)
+            {
+                notaBox.Text = string.Empty;
+            }
+            foreach (TextBox ponderacionBox in textBoxPonderaciones)
+            {
+                ponderacionBox.Text = string.Empty;
+            }
+
+            // Habilitar el botón de agregar nuevamente (si se había deshabilitado)
+            button1.Enabled = true;
+
+            // Limpia cualquier otro control que desees restablecer
+            txtPromedio.Text = string.Empty;
+        }
+    }
+}
